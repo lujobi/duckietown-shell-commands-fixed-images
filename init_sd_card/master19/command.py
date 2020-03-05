7f38b33635ecc0bce80faf2664711b484b23a1a7
Luzian Bieri <luzibier@student.ethz.ch>
2020-03-05 10:58:36 +0100

    added more depth to init_sd_card

---
+++ init_sd_card/master19/command.py
@@ -0,0 +1,11 @@
+from dt_shell import DTCommandAbs
+
+from dt_shell import DTShell
+
+
+class DTCommand(DTCommandAbs):
+    help = "Shows information about the current project"
+
+    @staticmethod
+    def command(shell: DTShell, args):
+        print("sdflöflöcövcv.-mcvb")
